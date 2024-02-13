ea20decc51be74208410dd2a3746f7695f1f80de
jay-brdy <jaysirit@gmail.com>
2024-02-12 21:15:25 -0600

    broke down navbar and main container into seperate containers

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,14 @@
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
-import Home from "./components/Home";
-import Blue from "./components/Blue";
-import Red from "./components/Red";
-import Green from "./components/Green";
-import Yellow from "./components/Yellow";
+import Navbar from "./components/Navbar";
+import Maincontainer from "./components/Maincontainer";
 
 export default function App() {
 
   return (
     <>
       <div id="container">
-        <div id="navbar">
-          <Link to="/">Home</Link>
-          <Link to="/blue">Blue</Link>
-          <Link to="/red">Red</Link>
-          <Link to="/green">Green</Link>
-          <Link to="/yellow">Yellow</Link>
-        </div>
-
-        <div id="main-section">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/blue" element={<Blue />} />
-            <Route path="/red" element={<Red />} />
-            <Route path="/green" element={<Green />} />
-            <Route path="/yellow" element={<Yellow />} />
-          </Routes>
-        </div>
+        <Navbar />
+        <Maincontainer />
       </div>
     </>
   );
